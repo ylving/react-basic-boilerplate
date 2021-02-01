83d3f379ecf181b0640e4376e59e10f5c4718ff1
Christer Aarø <christeraaro@gmail.com>
2021-02-01 11:43:19 +0100

    Webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './app.css';
+
+const App = () => (
+  <div className="app">
+    <h1>Hello React!</h1>
+  </div>
+)
+
+export default hot(module)(App);
